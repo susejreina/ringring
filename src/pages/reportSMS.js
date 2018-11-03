39053f1224851142b154c06d84fb1a0ee7761287
gracielarangel <31774336+gracielarangel@users.noreply.github.com>
2018-11-02 23:51:08 -0600

    cambios
    mejora

--- src/pages/reportSMS.js
+++ src/pages/reportSMS.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Panel from '../components/panel.js';
+import FlatButton from 'material-ui/FlatButton';
+import DatePicker from 'material-ui/DatePicker';
 import ReportsSMS from '../components/tables/reportSMS.js';
 
 const ReportSms = () => (
   <div>
   <h3>SMS REPORT</h3>
     <Panel title="Lists messages">
+      <div>
+        Date range: <DatePicker id="date_since" hintText="Since" container="inline" /> <DatePicker id="date_to" hintText="To" container="inline" /> <FlatButton label="Search" />
+
+      </div>
+      <br />
       <ReportsSMS striped={true}/>
     </Panel>
   </div>
