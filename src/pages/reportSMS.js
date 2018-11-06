ff2ad6c4f27de90ba698f044a562913ea84f9043
susejreina <susej@x8bit.com>
2018-11-05 19:12:10 -0600

    Merge del archivo

--- src/pages/reportSMS.js
+++ src/pages/reportSMS.js
@@ -8,7 +8,6 @@ const ReportSms = () => (
   <div>
   <h3>SMS REPORT</h3>
     <Panel title="Lists messages">
-<<<<<<< HEAD
       <div className="row">
         <div className="col-xs-12 col-sm-12 col-md-3">
           Date range:
@@ -22,11 +21,6 @@ const ReportSms = () => (
         <div className="col-xs-12 col-sm-12 col-md-3">
           <FlatButton label="Search" />
         </div>
-=======
-      <div>
-        Date range: <DatePicker id="date_since" hintText="Since" container="inline" /> <DatePicker id="date_to" hintText="To" container="inline" /> <FlatButton label="Search" />
-
->>>>>>> 75bf656f7b2a3d0f4482fec4ebc910246941f8c6
       </div>
       <br />
       <ReportsSMS striped={true}/>
